051229b3ece3f1a84e53ff275dd726fdb30ffa6d
timhagn <mail@timhagn.com>
2018-10-11 10:59:32 +0200

    update github auth with new github profile api

--- server.js
+++ server.js
@@ -73,7 +73,8 @@ mongo.connect(process.env.DATABASE, { useNewUrlParser: true }, (err, client) => 
                       id: profile.id,
                       name: profile.displayName || 'John Doe',
                       photo: profile.photos[0].value || '',
-                      email: profile.emails[0].value || 'No public email',
+                      // Error in original! profile.emails[0].value doesn't exist!
+                      email: profile.email || 'No public email',
                       created_on: new Date(),
                       provider: profile.provider || ''
                     },$set:{
