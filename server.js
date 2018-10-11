fbcb00d53da5bb97cc9a6f8d5dcfac91cd4af7b1
timhagn <mail@timhagn.com>
2018-10-11 10:51:42 +0200

    update github auth with (deprecated) logic

--- server.js
+++ server.js
@@ -66,7 +66,26 @@ mongo.connect(process.env.DATABASE, { useNewUrlParser: true }, (err, client) => 
             },
             function(accessToken, refreshToken, profile, cb) {
               console.log(profile);
-              //Database logic here with callback containing our user object
+              db.collection('socialusers').findAndModify(
+                  {id: profile.id},
+                  {},
+                  {$setOnInsert:{
+                      id: profile.id,
+                      name: profile.displayName || 'John Doe',
+                      photo: profile.photos[0].value || '',
+                      email: profile.emails[0].value || 'No public email',
+                      created_on: new Date(),
+                      provider: profile.provider || ''
+                    },$set:{
+                      last_login: new Date()
+                    },$inc:{
+                      login_count: 1
+                    }},
+                  {upsert:true, new: true},
+                  (err, doc) => {
+                    return cb(null, doc.value);
+                  }
+              );
             }
         ));
 
